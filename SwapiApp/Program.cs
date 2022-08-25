a33127016e03029b2ec128b3e0a51a6cac5c2fb3
Noreldin.Nimer <Noreldin.Nimer@NON-NB>
2022-08-26 00:10:56 +0200

    GraphQL and Hot Chocolate integration

--- SwapiApp/Program.cs
+++ SwapiApp/Program.cs
@@ -1,5 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Register the GraphQL on the external API serivce
+builder.Services
+    .AddGraphQLServer();
+
 var app = builder.Build();
+
+// Initialize the GraphQL service
+app.MapGraphQL();
 
 app.MapGet("/", () => "Hello World!");
 

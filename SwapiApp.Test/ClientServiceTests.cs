66425a4821857d9333ccead3356a95ef4f1328dc
Noreldin.Nimer <Noreldin.Nimer@NON-NB>
2022-08-26 02:07:01 +0200

    Adding service unit testing with moq

---
+++ SwapiApp.Test/ClientServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using Moq;
+using SwapiApp.Models;
+using SwapiApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SwapiApp.Test
+{
+    public class ClientServiceTests
+    {
+        public static IEnumerable<object[]> PersonData => new List<Person[]>
+        {
+            new Person[] {new Person
+            {
+                BirthYear = "1990",
+                EyeColor = "Blue",
+                Name ="James",
+                Id = 1
+            }},
+            new Person[] {new Person
+            {
+                BirthYear = "1990",
+                EyeColor = "Blue",
+                Name ="James",
+                Id = 2
+            }},
+            new Person[] {new Person
+            {
+                BirthYear = "1990",
+                EyeColor = "Blue",
+                Name ="James",
+                Id = 3
+            }},
+            new Person[] {new Person
+            {
+                BirthYear = "1990",
+                EyeColor = "Blue",
+                Name ="James",
+                Id = 4
+            }},
+        }.ToArray();
+
+        [Theory]
+        [MemberData(nameof(PersonData))]
+        public async void Test_GetPersonFromApiById(Person person)
+        {
+            //Arrange
+            var mockedService = new Mock<IClientService>();
+
+            mockedService.Setup(t => t.GetPersonFromApiById(person.Id)).Returns(async () => await Task<Person>.FromResult(person));
+
+            var service = mockedService.Object;
+
+            //Act
+            var result = await service.GetPersonFromApiById(person.Id);
+
+            //Assert
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ SwapiApp.Test/DatabaseFixture.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SwapiApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapiApp.Test
+{
+    internal class DatabaseFixture : IDisposable
+    {
+        public DatabaseFixture()
+        {
+            var collection = new ServiceCollection();
+            collection.AddPooledDbContextFactory<DataContext>(opt => opt.UseInMemoryDatabase(databaseName: "People"));
+
+            var serviceProvider = collection.BuildServiceProvider();
+
+            this.ContextFactory = serviceProvider.GetRequiredService<IDbContextFactory<DataContext>>();
+        }
+
+        public void Dispose()
+        {
+            //nothing.
+        }
+
+        public IDbContextFactory<DataContext> ContextFactory { get; private set; }
+    }
+}
